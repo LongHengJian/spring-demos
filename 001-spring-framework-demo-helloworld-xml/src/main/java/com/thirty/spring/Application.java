af604dfb33a802e3c7ced73314873e9c0bdd4039
LHJ <longhengjian@utry.cn>
2022-04-14 22:40:19 +0800

    feat: 1. spring-hello-world项目结构搭建

---
+++ 001-spring-framework-demo-helloworld-xml/src/main/java/com/thirty/spring/Application.java
@@ -0,0 +1,11 @@
+package com.thirty.spring;
+
+/**
+ * @author 龙恒建
+ * @Email longhengjian@utry.cn
+ * @date 2022-04-14 22:37
+ * @ClassName Application
+ * @description: 启动类
+ */
+public class Application {
+}
